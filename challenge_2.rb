c95091b76dfd17519ce43a4ca294dcba91858bf7
Anupam Basu <nupebasu@gmail.com>
2014-04-28 09:39:51 -0500

    challenge_2.rb

--- challenge_2.rb
+++ challenge_2.rb
@@ -1,4 +1,4 @@
-require './challenge_1.rb'
+# require './challenge_1.rb'
 # Line 1 effectively copy-pastes in any code you wrote in challenge_1.rb
 # It's similar to connecting an external stylesheet to an HTML file.
 # So if you've already implemented the pmt method over there, then you don't have to rewrite it here.
@@ -8,7 +8,6 @@ require './challenge_1.rb'
 # ===========
 
 # Below is an array of hashes. Each hash in the array represents one rate quote from a bank.
-
 loan_quotes = [
   { "bank" => "Elm Street Bank", "rate" => 0.013},
   { "bank" => "Oak Street Bank", "rate" => 0.009},
@@ -25,5 +24,27 @@ principal_amount = 100000.0
 #   Re-use the pmt method that you defined in Challenge 1.
 
 # ====================
-# Your code goes here.
+nper = 180
+pv = 100000.0
+
+loan_quotes = [
+  { "bank" => "Elm Street Bank", "rate" => 0.013},
+  { "bank" => "Oak Street Bank", "rate" => 0.009},
+  { "bank" => "Ash Street Bank", "rate" => 0.011}
+]
+
+puts "Based on the information you entered regarding your loan;"
+
+x=0
+loan_quotes.each do |quote|
+  rate= loan_quotes[x]["rate"].to_f
+  i=(1+rate/12)**(12/12)-1
+
+  annuity=(1-(1/(1+i))**nper)/i
+
+  pmt=pv/annuity
+puts "Your monthly payments will be $#{pmt.to_i}" + " for #{loan_quotes[x]["bank"]}"
+x=x+1
+end
+
 # ====================
